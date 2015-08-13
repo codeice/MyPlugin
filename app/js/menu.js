81e2a1a0b56b6b5f20d54557fa5c8b9143b9dcaa
codeice <lyb.jessie@hotmail.com>
2015-08-13 12:05:19 +0800

    菜单构建
    菜单构建

---
+++ app/js/menu.js
@@ -0,0 +1,46 @@
+/**
+ * @authors Your Name (you@example.org)
+ * @date    2015-08-13 10:34:40
+ * @version $Id$
+ */
+var menuConfig = {
+    "menus": [{
+        "name": "一级菜单1",
+        "url": "http://www.baidu.com",
+        "subMenus": [{
+            "name": "二级菜单11",
+            "url": "http://www.baidu.com",
+            "subMenus": [{
+                "name": "三级菜单31",
+                "url": "http://www.baidu.com"
+            }, {
+                "name": "三级菜单32",
+                "url": "http://www.baidu.com"
+            }]
+
+        }, {
+            "name": "二级菜单11",
+            "url": "http://www.baidu.com"
+        }, {
+            "name": "二级菜单12",
+            "url": "http://www.baidu.com"
+        }]
+    }, {
+        "name": "一级菜单2",
+        "url": "http://www.baidu.com",
+        "subMenus": [{
+            "name": "二级菜单21",
+            "url": "http://www.baidu.com"
+
+        }, {
+            "name": "二级菜单22",
+            "url": "http://www.baidu.com"
+        }, {
+            "name": "二级菜单23",
+            "url": "http://www.baidu.com"
+        }]
+    }, {
+        "name": "一级菜单3",
+        "url": "http://www.baidu.com"
+    }]
+};
---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @authors Your Name (you@example.org)
+ * @date    2015-08-12 18:23:53
+ * @version $Id$
+ */
+
+var gulp=require('gulp');
+gulp.task('default',function(){
+
+});
