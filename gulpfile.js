3caa6ab048b4d4a69797de3a87e4d5d060ec5f85
jessie <lyb.jessie@hotmail.com>
2016-03-02 09:34:32 +0800

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,49 @@
 /**
- *
+ * 
  * @authors Your Name (you@example.org)
- * @date    2015-08-12 18:23:53
+ * @date    2016-03-01 15:03:42
  * @version $Id$
  */
 
- var gulp=require('gulp');
- gulp.task('default',function(){
+var  gulp=require('gulp');
+var browserSync=require('browser-sync').create();
+var less=require('gulp-less');
+var cssmin=require('gulp-cssmin');
+var rename=require('gulp-rename');
+var uglify =require('gulp-uglify');
 
- });
+gulp.task('browser-sync',function(){
+	browserSync.init({
+		server:{baseDir:'./src'}
+	});
+});
+
+//---less编译,css minify
+gulp.task('less',function(){
+	return gulp.src('./src/css/*.less')
+	.pipe(less().on('error',function(err){
+		console.log("compile less error=",err);
+	}))
+	.pipe(cssmin())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('./dest/css'));
+});
+
+//---js压缩
+gulp.task('uglify-js',function(){
+	return
+	 gulp.src('./src/js/*.js')
+	.pipe(uglify())
+	.pipe(rename({suffix:'.min'}))
+	.pipe(gulp.dest('./dest/js'));
+});
+
+
+gulp.task('default',['browser-sync','compile-less','uglify-js'],function(){
+	console.log("hello gulp...");
+	gulp.watch('./src/css/*.less',['less']);
+/*	gulp.src('./src/*.html')
+	.pipe(gulp.dest('./dest/*.html'));*/
+});	
+
+
